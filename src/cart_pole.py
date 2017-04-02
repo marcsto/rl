90a78f2f4eef9be6deaf7508840ad4a47de9c1ac
marcsto <marcsto@gmail.com>
2017-04-01 23:04:51 -0700

    Simplify cart_pole so that it can be used as a learning example.

--- src/cart_pole.py
+++ src/cart_pole.py
@@ -35,19 +35,9 @@ def computeReward(agent):
                 all_run_reward_sum += reward_sum
                 break
     return all_run_reward_sum / EVAL_RUNS
-
-""" An agent takes an action based on observations """
-class Agent:
-    def take_action(self, observation):
-        raise NotImplementedError()
-
-""" Randomly selects an action. Useful for testing """
-class RandomAgent(Agent):
-    def take_action(self, observation):
-        return env.action_space.sample()
     
 """ Chooses between 2 actions based on a set of weights """
-class BinaryActionAgent(Agent):
+class BinaryActionAgent():
     def __init__(self, weights):
         self.weights = weights
 
@@ -59,7 +49,7 @@ class BinaryActionAgent(Agent):
     Algorithm description: 
     1. Create a population of weight vectors by making random modification to an initial weight vector.
     2. Compute the reward for each weight vector
-    3. Move the main weight vector towards the weight vectors that had the most success.
+    3. Adjust the main weight vector towards the weight vectors that had the most success.
     4. Repeat the process above.
     
     https://blog.openai.com/evolution-strategies/  
@@ -71,32 +61,32 @@ class NaturalEvolutionOptimizer:
         sigma = 0.1  # noise standard deviation  
         alpha = 0.1  # learning rate
           
-        w = np.random.randn(observation_size)  # initial guess  
+        # initial guess is a random vector
+        w = np.random.randn(observation_size)  
         for i in range(iterations):
             print("Starting iteration ", i)
             N = np.random.randn(npop, observation_size)
             R = np.zeros(npop)
+            # Iterate over every member of the new population
             for j in range(npop):
+                # Add noise to the vector. This generates a new candidate.
                 w_try = w + sigma * N[j]
                 agent = BinaryActionAgent(w_try)
-                R[j] = computeReward(agent)  # f(w_try)
+                # See how well the new candidate performs.
+                R[j] = computeReward(agent)
                 print(R[j], w_try, j, i)
-            
+
             stdDev = np.std(R)
             if (stdDev == 0):
                 break
+            # Adjust the main weight vector towards the weight vectors that had the most success.
             A = (R - np.mean(R)) / np.std(R)
             w = w + alpha / (npop * sigma) * np.dot(N.T, A)
         
         print(w)
         return BinaryActionAgent(w)
-    
-        
-    
+
 if __name__ == '__main__':
     best_agent = NaturalEvolutionOptimizer().optimize()
     final_reward = computeReward(best_agent)
     print("Final reward = ", final_reward)
-    
-
-
